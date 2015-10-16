921adb5377850cade5561d2823463b5d4878eb89
Marco Livraghi <mrclivraghi@gmail.com>
2015-10-16 22:58:15 +0200

    custom time serializer

---
+++ src/main/java/it/polimi/utils/CustomJsonTimeDeserializer.java
@@ -0,0 +1,37 @@
+package it.polimi.utils;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class CustomJsonTimeDeserializer extends JsonDeserializer<Time>
+{
+	public  CustomJsonTimeDeserializer ()
+	{
+	}
+    @Override
+    public Time deserialize(JsonParser jsonparser,
+            DeserializationContext deserializationcontext) throws IOException, JsonProcessingException {
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        String time = jsonparser.getText();
+        try {
+            Date date=format.parse(time);
+            Time returnedTime = new Time(date.getTime());
+            return returnedTime;
+        } catch (ParseException e) {
+        	
+            		throw new RuntimeException(e);
+            
+        }
+
+    }
+
+}

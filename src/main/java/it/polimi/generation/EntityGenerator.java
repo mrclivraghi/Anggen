9bb6908d20eb51cd2315bb5ddf9e5965d2176b24
Marco Livraghi <mrclivraghi@gmail.com>
2015-11-22 23:25:17 +0100

    wip entity generator with annotation

--- src/main/java/it/polimi/generation/EntityGenerator.java
+++ src/main/java/it/polimi/generation/EntityGenerator.java
@@ -1,6 +1,8 @@
 package it.polimi.generation;
 
+import it.polimi.boot.OracleNamingStrategy;
 import it.polimi.model.domain.Entity;
+import it.polimi.model.domain.EntityAttribute;
 import it.polimi.model.domain.Field;
 import it.polimi.model.domain.Relationship;
 import it.polimi.model.domain.RelationshipType;
@@ -13,7 +15,20 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
 import com.sun.codemodel.ClassType;
+import com.sun.codemodel.JAnnotationUse;
 import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JClassAlreadyExistsException;
@@ -101,28 +116,104 @@ public class EntityGenerator {
 			e.printStackTrace();
 		}
 		myClass.annotate(javax.persistence.Entity.class);
+		JAnnotationUse annotationTable= myClass.annotate(Table.class);
+		//from properties
+		annotationTable.param("schema", "public");
+		OracleNamingStrategy namingStrategy = new OracleNamingStrategy();
+		annotationTable.param("name", namingStrategy.classToTableName(entity.getName()));
 		for (Field field : entity.getFieldList())
 		{
 			Class fieldClass = getFieldClass(field);
 			JVar classField = myClass.field(JMod.PRIVATE, fieldClass, field.getName());
+			JAnnotationUse columnAnnotation = classField.annotate(Column.class);
+			columnAnnotation.param("name", namingStrategy.classToTableName(field.getName()));
 			generateGetterAndSetter(myClass, field.getName(), fieldClass);
+			addValidationAnnotation(field,classField);
 		}
 		for (Relationship relationship : entity.getRelationshipList())
 		{
+			JVar listField;
+			
 			if (relationship.isList())
 			{
 				JClass listClass = codeModel.ref(List.class).narrow(Generator.getJDefinedClass(relationship.getEntityTarget().getName()));
-				JVar listField = myClass.field(JMod.PRIVATE, listClass, relationship.getEntityTarget().getName()+"List");
+				listField = myClass.field(JMod.PRIVATE, listClass, relationship.getEntityTarget().getName()+"List");
 				generateGetterAndSetter(myClass, relationship.getEntityTarget().getName()+"List", listClass);
+				if (relationship.getRelationshipType()==RelationshipType.ONE_TO_MANY)
+				{
+					JAnnotationUse oneToMany = listField.annotate(OneToMany.class);
+					oneToMany.param("fetch", FetchType.EAGER);
+					JAnnotationUse type = listField.annotate(Type.class);
+					type.param("type", Generator.getJDefinedClass(relationship.getEntityTarget().getName()).fullName());
+					JAnnotationUse joinColumn = listField.annotate(JoinColumn.class);
+					joinColumn.param("name", namingStrategy.classToTableName(relationship.getEntityTarget().getName())+"_id_"+namingStrategy.classToTableName(relationship.getEntityTarget().getName()));
+				}
+
+				if (relationship.getRelationshipType()==RelationshipType.MANY_TO_MANY)
+				{
+				    /*@ManyToMany(fetch=FetchType.EAGER)
+				    @Type(type="it.polimi.boot.domain.UserRole")
+				    @JoinTable(name="user_role", schema="sso", joinColumns = {
+				            @JoinColumn(name="user_id") },
+				            inverseJoinColumns= {
+				            @JoinColumn(name="role_id")
+				            
+				    })*/
+					JAnnotationUse manyToMany = listField.annotate(ManyToMany.class);
+					manyToMany.param("fetch", FetchType.EAGER);
+					JAnnotationUse type = listField.annotate(Type.class);
+					type.param("type", Generator.getJDefinedClass(relationship.getEntityTarget().getName()).fullName());
+					JAnnotationUse joinTable = listField.annotate(JoinTable.class);
+					joinTable.param("name", namingStrategy.classToTableName(relationship.getEntity().getName())+"_"+namingStrategy.classToTableName(relationship.getEntityTarget().getName()));
+					joinTable.param("schema", "public");
+					joinTable.param("joinColumns","");
+					joinTable.param("inverseJoinColumns", "");
+					
+
+				}
+				if (relationship.getRelationshipType()==RelationshipType.MANY_TO_MANY_BACK)
+				{
+					JAnnotationUse manyToMany = listField.annotate(ManyToMany.class);
+					manyToMany.param("fetch", FetchType.EAGER);
+					manyToMany.param("mappedBy", entity.getName()+"List");
+				}
+
+				
+				
 			}
 			else
 			{
-				JVar listField = myClass.field(JMod.PRIVATE, Generator.getJDefinedClass(relationship.getEntityTarget().getName()), relationship.getEntityTarget().getName());
+				listField = myClass.field(JMod.PRIVATE, Generator.getJDefinedClass(relationship.getEntityTarget().getName()), relationship.getEntityTarget().getName());
+				if (relationship.getRelationshipType()==RelationshipType.MANY_TO_ONE)
+				{
+					JAnnotationUse manyToOne = listField.annotate(ManyToOne.class);
+					manyToOne.param("fetch", FetchType.EAGER);
+					JAnnotationUse joinColumn = listField.annotate(JoinColumn.class);
+					joinColumn.param("name", namingStrategy.classToTableName(relationship.getEntityTarget().getName())+"_id_"+namingStrategy.classToTableName(relationship.getEntityTarget().getName()));
+				
+				}
+				if (relationship.getRelationshipType()==RelationshipType.ONE_TO_ONE)
+				{
+					JAnnotationUse oneToOne = listField.annotate(OneToOne.class);
+					oneToOne.param("fetch", FetchType.EAGER);
+					JAnnotationUse type = listField.annotate(Type.class);
+					type.param("type", Generator.getJDefinedClass(relationship.getEntityTarget().getName()).fullName());
+					JAnnotationUse joinColumn = listField.annotate(JoinColumn.class);
+					joinColumn.param("name", namingStrategy.classToTableName(relationship.getEntityTarget().getName())+"_id_"+namingStrategy.classToTableName(relationship.getEntityTarget().getName()));
+				
+				}
 				generateGetterAndSetter(myClass, relationship.getEntityTarget().getName(), Generator.getJDefinedClass(relationship.getEntityTarget().getName()));
 			}
+			addValidationAnnotation(relationship,listField);
 		}
 		saveFile(codeModel);
 		return myClass;
+	}
+
+
+	private void addValidationAnnotation(EntityAttribute entityAttribute, JVar classField) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
--- src/main/java/it/polimi/model/domain/RelationshipType.java
+++ src/main/java/it/polimi/model/domain/RelationshipType.java
@@ -4,7 +4,8 @@ public enum RelationshipType {
 	ONE_TO_ONE(0),
 	ONE_TO_MANY(1),
 	MANY_TO_ONE(2),
-	MANY_TO_MANY(3);
+	MANY_TO_MANY(3),
+	MANY_TO_MANY_BACK(4);
 	
 	private final int value;
 	

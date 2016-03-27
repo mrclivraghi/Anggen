d56ad61ab4de122823692ae7b6d23a2f9c431434
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-03-27 20:56:08 +0200

    upgrade utility js

--- src/main/webapp/js/utility.js
+++ src/main/webapp/js/utility.js
@@ -60,3 +60,45 @@ var AlertError = (function() {
     return that;
 }());
 
+
+function cloneObject(sourceObject,targetObject)
+{
+	 
+	var keyList = Object.keys(sourceObject);
+	if (keyList.length == 0)
+		keyList = Object.keys(targetObject);
+	for (i = 0; i < keyList.length; i++) {
+		var val = keyList[i];
+		if (val != undefined) {
+			if (val.toLowerCase().indexOf("list") > -1
+					&& (typeof sourceObject[val] == "object" || typeof targetObject[val]=="object")) {
+				if (sourceObject[val] != null
+						&& sourceObject[val] != undefined) {
+					if (targetObject[val]!=undefined)
+						while (targetObject[val].length > 0)
+							targetObject[val].pop();
+					if (sourceObject[val] != null)
+						for (j = 0; j < sourceObject[val].length; j++)
+							targetObject[val]
+					.push(sourceObject[val][j]);
+				} else 
+					emptyList(targetObject[val]);
+			} else {
+				if (val.toLowerCase().indexOf("time") > -1
+						&& typeof val == "string") {
+					var date = new Date(sourceObject[val]);
+					targetObject[val] = new Date(sourceObject[val]);
+				} else {
+					targetObject[val] = sourceObject[val];
+				}
+			}
+		}
+	};
+
+}
+
+function emptyList(list)
+{
+	while (list.length>0)
+		list.pop();
+}

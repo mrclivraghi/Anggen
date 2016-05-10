ed7bf9b501aecd26aad4a0d595d8d33567fbd1c4
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-05-10 16:18:03 +0200

    not in feature

--- src/main/java/it/anggen/generation/Generator.java
+++ src/main/java/it/anggen/generation/Generator.java
@@ -349,7 +349,7 @@ public class Generator {
 
 			// Create a branch
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
-			generationBranchName="refs/heads/feature/generation/gen_"+sdf.format(new Date());
+			generationBranchName="refs/heads/generation/gen_"+sdf.format(new Date());
 			Ref testBranch= repo.getRef(generationBranchName);
 			if (testBranch==null)
 			{

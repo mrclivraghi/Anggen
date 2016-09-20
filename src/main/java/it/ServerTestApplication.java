fce009ee80516e681f04b08f249937926da7f599
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-09-20 23:06:46 +0200

    date serializer

--- src/main/java/it/ServerTestApplication.java
+++ src/main/java/it/ServerTestApplication.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class ServerTestApplication {
 
 
--- src/main/java/it/anggen/utils/HibernateAwareObjectMapper.java
+++ src/main/java/it/anggen/utils/HibernateAwareObjectMapper.java
@@ -1,7 +1,11 @@
 package it.anggen.utils;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 public class HibernateAwareObjectMapper extends ObjectMapper {
@@ -14,5 +18,9 @@ public class HibernateAwareObjectMapper extends ObjectMapper {
     public HibernateAwareObjectMapper() {
         registerModule(new Hibernate4Module());
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        final SimpleModule module = new SimpleModule("", Version.unknownVersion());
+        module.addSerializer(Date.class, new CustomDateSerializer());
+        registerModule(module);
+        
     }
 }
